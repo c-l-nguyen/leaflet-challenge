6d84f274333b090b558941e2ceec529b22aea0ea
c-l-nguyen <c.l.nguyen@icloud.com>
2020-12-23 00:03:42 -0600

    personal throwaway api key

--- static/js/config.js
+++ static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "pk.eyJ1IjoibWFubGFyYSIsImEiOiJjamtnd2R3ZW4wbWlwM3FxZ3BoY2JkNm1qIn0.NQ7yhFgYEP0fqdHkT9c6-Q";
+const API_KEY = "pk.eyJ1Ijoib3BlbmFwaWtleSIsImEiOiJja2owejNkaXEwaWRsMzBsbmZsMHc4NzlkIn0.NDkF3yzjQc5QFMEAVn_1nw";
